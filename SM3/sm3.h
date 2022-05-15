bec512c65e97813be9c38271f986c1c57f686542
sdu-lzq <72809691+sdu-lzq@users.noreply.github.com>
2022-05-15 20:01:23 +0800

    Add files via upload

---
+++ SM3/sm3.h
@@ -0,0 +1,17 @@
+/*This is the head file of sm3.*/
+
+#define SM3_H
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
